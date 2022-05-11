f79a477015dc4f7be3ef6529e08f69052a65a886
Luis Pérez <luis.perez@geekdacity.com>
2022-05-10 22:23:02 -0600

    feat: Create resizer utility

---
+++ src/utilities/resizer.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import sharp from 'sharp';
+import path from 'path';
+
+const resizer = async(req: express.Request, res: express.Response, next: Function) => {
+    const filename = req.query.filename || null;
+    const newWidth = req.query.width || 0;
+    const newHeight = req.query.height || 0;
+    const fullImage = `images/full/${filename}.jpg`;
+    const resizedImage = `images/thumb/${filename}_thumb_${newWidth}x${newHeight}.jpg`;
+
+    try {
+        await sharp(fullImage)
+            .resize(+newWidth, +newHeight)
+            .toFile(resizedImage);
+
+        res.sendFile(resizedImage, { root: './' });
+    } catch (error) {
+        res.status(600).send('error');
+    }
+
+    next();
+};
+
+export default resizer;
