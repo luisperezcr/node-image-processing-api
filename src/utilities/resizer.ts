69508c40232175c7b67ff20197e625e6f934aa32
Luis Pérez <luis.perez@geekdacity.com>
2022-05-11 00:20:36 -0600

    feat: Implement cache

--- src/utilities/resizer.ts
+++ src/utilities/resizer.ts
@@ -1,24 +1,36 @@
 import express from 'express';
 import sharp from 'sharp';
+import { isInCache, saveToCache } from './cache';
 
-const resizer = async(req: express.Request, res: express.Response, next: Function) => {
-    const filename = req.query.filename || null;
-    const newWidth = req.query.width || 0;
-    const newHeight = req.query.height || 0;
-    const fullImage = `images/full/${filename}.jpg`;
-    const resizedImage = `images/thumb/${filename}_thumb_${newWidth}x${newHeight}.jpg`;
+const resizerMiddleware = async(req: express.Request, res: express.Response, next: Function) => {
+  const filename = req.query.filename || null;
+  const newWidth = req.query.width || 0;
+  const newHeight = req.query.height || 0;
 
-    try {
-        await sharp(fullImage)
-            .resize(+newWidth, +newHeight)
-            .toFile(resizedImage);
+  try {
+    if (filename && newWidth && newHeight) {
+      const pathToFull = 'images/full';
+      const pathToThumb = 'images/thumb';
+      const fullImage = `${filename}.jpg`;
+      const thumbImage = `${filename}_thumb_${newWidth}x${newHeight}.jpg`;
 
-        res.sendFile(resizedImage, { root: './' });
-    } catch (error) {
-        res.status(600).send('error');
+      const isCached = await isInCache(thumbImage);
+      if (isCached) {
+        res.sendFile(`${pathToThumb}/${thumbImage}`, { root: './' });
+      } else {
+        await sharp(`${pathToFull}/${fullImage}`)
+          .resize(+newWidth, +newHeight)
+          .toFile(`${pathToThumb}/${thumbImage}`);
+        
+        await saveToCache(thumbImage);
+        res.sendFile(`${pathToThumb}/${thumbImage}`, { root: './' });
+      }
     }
+  } catch (error) {
+      res.status(404).send(`Sorry, we couldn't find an image with that name.`);
+  }
 
-    next();
+  next();
 };
 
-export default resizer;
+export default resizerMiddleware;
